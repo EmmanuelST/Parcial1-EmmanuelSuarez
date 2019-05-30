109836f933f62148208f0a7f8d6ac335f919a40c
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-05-30 10:25:51 -0400

    Agragando el metodo guardar del ProductoBLL

---
+++ Parcial1-EmmanuelSuarez/BLL/ProductoBLL.cs
@@ -0,0 +1,38 @@
+﻿using Parcial1_EmmanuelSuarez.DAL;
+using Parcial1_EmmanuelSuarez.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_EmmanuelSuarez.BLL
+{
+    class ProductoBLL
+    {
+
+        public static bool Guardar(Productos producto)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if(db.Producto.Add(producto) != null)
+                {
+                    paso = (db.SaveChanges() > 0);
+                }
+
+            }catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+    }
+}
