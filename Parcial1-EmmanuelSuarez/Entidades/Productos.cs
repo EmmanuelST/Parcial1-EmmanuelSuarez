1885ab5d45f1104735566efe167f333030b36452
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-05-30 09:27:43 -0400

    Agregando la Entidad Producto

---
+++ Parcial1-EmmanuelSuarez/Entidades/Productos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_EmmanuelSuarez.Entidades
+{
+    class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public float Costo { get; set; }
+        
+        public Productos()
+        {
+            ProductoId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0.0f;
+        }
+    }
+}
