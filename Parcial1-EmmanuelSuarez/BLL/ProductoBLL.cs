ce54bef53ea736573d090b2c197337f92f0288ef
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-05-30 11:02:20 -0400

    Agragando el restos de metodos de la clase ProductoBLL

--- Parcial1-EmmanuelSuarez/BLL/ProductoBLL.cs
+++ Parcial1-EmmanuelSuarez/BLL/ProductoBLL.cs
@@ -2,6 +2,7 @@
 using Parcial1_EmmanuelSuarez.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +36,71 @@ namespace Parcial1_EmmanuelSuarez.BLL
             return paso;
         }
 
+        public static bool Modificar(Productos producto)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+
+                db.Entry(producto).State = EntityState.Modified;
+                paso =(db.SaveChanges() > 0);
+
+            }catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static Productos Buscar(int id)
+        {
+            Contexto db = new Contexto();
+            Productos producto = new Productos();
+
+            try
+            {
+                producto = db.Producto.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return producto;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                var eliminar = db.Producto.Find(id);
+                db.Entry(eliminar).State = EntityState.Deleted;
+                paso = (db.SaveChanges() > 0);
+
+            }catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
 
     }
 }
