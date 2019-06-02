3b60744bc07d2847011575ad99bba84b70160d1a
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-01 21:21:37 -0400

    Subiendo la clase BLL para el registro de modificaciones

---
+++ Parcial1-EmmanuelSuarez/BLL/ProductoModificacionBLL.cs
@@ -0,0 +1,39 @@
+﻿using Parcial1_EmmanuelSuarez.DAL;
+using Parcial1_EmmanuelSuarez.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_EmmanuelSuarez.BLL
+{
+    class ProductoModificacionBLL
+    {
+
+        public static bool Guardar(ProductoModificacion producto)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+               if(db.ProductoModificacion.Add(producto) != null)
+                {
+                    paso = db.SaveChanges() > 0;
+                }
+
+            }catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+        
+      
+    }
+}
