b6b60e1348bdac00e025de7cc48579e34dcb1863
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-03 18:58:03 -0400

    Creo que termine con las ubicaciones

---
+++ Parcial1-EmmanuelSuarez/UI/Registro/rUbicaciones.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Parcial1_EmmanuelSuarez.UI.Registro
+{
+    partial class rUbicaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IdnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DescripciontextBox = new System.Windows.Forms.TextBox();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id:";
+            // 
+            // IdnumericUpDown
+            // 
+            this.IdnumericUpDown.Location = new System.Drawing.Point(84, 22);
+            this.IdnumericUpDown.Name = "IdnumericUpDown";
+            this.IdnumericUpDown.Size = new System.Drawing.Size(133, 20);
+            this.IdnumericUpDown.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Descripción:";
+            // 
+            // DescripciontextBox
+            // 
+            this.DescripciontextBox.Location = new System.Drawing.Point(84, 56);
+            this.DescripciontextBox.Name = "DescripciontextBox";
+            this.DescripciontextBox.Size = new System.Drawing.Size(228, 20);
+            this.DescripciontextBox.TabIndex = 3;
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.Image = global::Parcial1_EmmanuelSuarez.Properties.Resources.nuevo_img;
+            this.Nuevobutton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Nuevobutton.Location = new System.Drawing.Point(127, 82);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(75, 62);
+            this.Nuevobutton.TabIndex = 5;
+            this.Nuevobutton.Text = "Nuevo";
+            this.Nuevobutton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            this.Nuevobutton.Click += new System.EventHandler(this.Nuevobutton_Click);
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Image = global::Parcial1_EmmanuelSuarez.Properties.Resources.borrar_img;
+            this.Eliminarbutton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Eliminarbutton.Location = new System.Drawing.Point(237, 82);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(75, 62);
+            this.Eliminarbutton.TabIndex = 6;
+            this.Eliminarbutton.Text = "Eliminar";
+            this.Eliminarbutton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            this.Eliminarbutton.Click += new System.EventHandler(this.Eliminarbutton_Click);
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Image = global::Parcial1_EmmanuelSuarez.Properties.Resources.busqueda_pequeña;
+            this.Buscarbutton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Buscarbutton.Location = new System.Drawing.Point(237, 13);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(75, 34);
+            this.Buscarbutton.TabIndex = 7;
+            this.Buscarbutton.Text = "Buscar";
+            this.Buscarbutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            this.Buscarbutton.Click += new System.EventHandler(this.Buscarbutton_Click);
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Image = global::Parcial1_EmmanuelSuarez.Properties.Resources.guardar_img__1_;
+            this.Guardarbutton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Guardarbutton.Location = new System.Drawing.Point(15, 82);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(75, 62);
+            this.Guardarbutton.TabIndex = 4;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // rUbicaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 156);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.DescripciontextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IdnumericUpDown);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(342, 195);
+            this.MinimumSize = new System.Drawing.Size(342, 195);
+            this.Name = "rUbicaciones";
+            this.Text = "rUbicaciones";
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown IdnumericUpDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DescripciontextBox;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
---
+++ Parcial1-EmmanuelSuarez/UI/Registro/rUbicaciones.cs
@@ -0,0 +1,143 @@
+﻿using Parcial1_EmmanuelSuarez.BLL;
+using Parcial1_EmmanuelSuarez.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial1_EmmanuelSuarez.UI.Registro
+{
+    public partial class rUbicaciones : Form
+    {
+        public rUbicaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            
+            if (!validar())
+                return;
+
+            Ubicaciones ubicacion = new Ubicaciones();
+            ubicacion = LlenarClase();
+
+            try
+            {
+                if (UbicacionesBLL.Buscar((int)IdnumericUpDown.Value) != null)
+                {
+                    UbicacionesBLL.Modificar(ubicacion);
+                    MessageBox.Show("Modificado Exitosamente");
+                }
+                else
+                {
+                    if ((int)IdnumericUpDown.Value == 0)
+                    {
+                        UbicacionesBLL.Guardar(ubicacion);
+                        MessageBox.Show("Guardaro exitosamente");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo guardar");
+                    }
+                }
+
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Hubo un error en la operacion");
+            }
+        }
+
+        private Ubicaciones LlenarClase()
+        {
+            Ubicaciones ubicacion = new Ubicaciones();
+
+            DescripciontextBox.Text = DescripciontextBox.Text.Trim();
+
+            ubicacion.UbicacionId = (int)IdnumericUpDown.Value;
+            ubicacion.Descripcion = DescripciontextBox.Text;
+
+            return ubicacion;
+        }
+
+        private void LlenarCampos(Ubicaciones ubicacion)
+        {
+            IdnumericUpDown.Value = ubicacion.UbicacionId;
+            DescripciontextBox.Text = ubicacion.Descripcion;
+
+        }
+
+        private void Limpiar()
+        {
+            IdnumericUpDown.Value = 0;
+            DescripciontextBox.Text =string.Empty;
+        }
+        
+        private bool validar()
+        {
+            bool paso = true;
+
+            if(string.IsNullOrWhiteSpace(DescripciontextBox.Text))
+            {
+                errorProvider.SetError(DescripciontextBox,"Este campo no puede estar vacio");
+                paso = false;
+            }
+
+            return paso;
+        }
+
+        private void Nuevobutton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void Eliminarbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IdnumericUpDown.Value > 0)
+                {
+                    UbicacionesBLL.Eliminar((int)IdnumericUpDown.Value);
+                    MessageBox.Show("Eliminado correctamente", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    errorProvider.Clear();
+                    errorProvider.SetError(IdnumericUpDown, "Este campo no puede ser 0");
+                }
+                Limpiar();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("algo falló al intentar eliminar ", "Fallo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Buscarbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (UbicacionesBLL.Buscar((int)IdnumericUpDown.Value) != null)
+                {
+                    LlenarCampos(UbicacionesBLL.Buscar((int)IdnumericUpDown.Value));
+                }
+                else
+                {
+                    MessageBox.Show("No se encontro la ubicacion", "Fallo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception i)
+            {
+
+                MessageBox.Show("Hubo un Problema restaurando" + i.ToString(), "Fallo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
